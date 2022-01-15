b8451ae16c5936e51f2aa34626d223d422c1bcfa
UTKARSH GUPTA <32601577+rajutkarshgupt@users.noreply.github.com>
2022-01-15 21:09:52 +0530

    Create Two_Sum.cpp

---
+++ Two_Sum.cpp
@@ -0,0 +1,56 @@
+ /*
+ 
+ PROBLEM DESCRIPTION :
+ 
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+ */
+ 
+ /*
+ 
+ ALGORITHM :
+ 
+ The basic idea is to maintain a hash table for each element num in nums, using num as key and its index (0-based) as value. For each num, search for target - num in the hash table. If it is found and is not the same element as num, then we are done.
+
+The code is as follows. Note that each time before we add num to mp, we search for target - num first and so we will not hit the same element.
+ 
+ */
+ 
+ vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.find(target - nums[i]) != indices.end()) {
+                return {indices[target - nums[i]], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
+    }
